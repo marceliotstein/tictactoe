16b5eab6ed76541cd1d86a129ca6f131017ee42f
Marc Eliot Stein <marc.eliot.stein@gmail.com>
2016-05-15 08:43:13 -0400

    finished basic grid

--- t3.js
+++ t3.js
@@ -2,7 +2,7 @@
  * t3.js
  */
 
-//AAAcells = document.getElementsByClassName("t3left");
-//cell1 = AAAcells[0];
-//ch = cell1.height;
+document.getElementById('tryit').onclick=function(){
+  alert("clicked");
+}
 
